30d7bb31a913c1c1c211058a694a99df0d7075bc
sdcervi <sdcervi@gmail.com>
2022-03-22 12:37:10 -0700

    Finished first pass at profile page
    Basic feature set now implemented

--- js/header-footer.js
+++ js/header-footer.js
@@ -7,10 +7,10 @@ primaryNavContents += '<a class="navbar-brand me-lg-4" href="#"><img src="assets
 primaryNavContents += '<button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation"><i class="bi bi-list"></i></button>';
 primaryNavContents += '<div class="collapse navbar-collapse" id="navbarSupportedContent">';
 primaryNavContents += '<ul class="navbar-nav me-auto mt-2 mt-lg-0 mb-lg-0">';
-primaryNavContents += '<li class="nav-item me-lg-2"><a class="nav-link" href="./index.html">Home</a></li>';
-primaryNavContents += '<li class="nav-item me-lg-2"><a class="nav-link" href="./dashboard.html">Dashboard</a></li>';
-primaryNavContents += '<li class="nav-item me-lg-2"><a class="nav-link" href="./profile.html">Profile</a></li>';
-primaryNavContents += '<li class="nav-item me-lg-2"><a class="nav-link" href="./about.html">About</a></li>';
+primaryNavContents += '<li class="nav-item me-lg-2"><a class="nav-link" id="nav-index" href="./index.html">Home</a></li>';
+primaryNavContents += '<li class="nav-item me-lg-2"><a class="nav-link" id="nav-dashboard" href="./dashboard.html">Dashboard</a></li>';
+primaryNavContents += '<li class="nav-item me-lg-2"><a class="nav-link" id="nav-profile" href="./profile.html">Profile</a></li>';
+primaryNavContents += '<li class="nav-item me-lg-2"><a class="nav-link" id="nav-about" href="./about.html">About</a></li>';
 primaryNavContents += '</ul></div>';
 primaryNavContents += '<a href="./signin.html"><button class="btn btn-sm signin" id="signin-button" type="button">Sign in</button></a>';
 primaryNavContents += '</div>';
@@ -23,3 +23,13 @@ let footerContents = '<hr class="text-muted mx-4"><div class="container"><p>Desi
 footerContents += '<p>Like my work and want to support me? <a href="https://ko-fi.com/sdcervi" target="_blank">Buy me a coffee</a> or <a href="https://www.etsy.com/listing/1178395889/sport-medal-honeycomb-display-plaque" target="_blank">buy one of my display plaques for your shiny new medals</a>.</p></div>';
 
 footerDiv.innerHTML = footerContents;
+
+// Get the active page and set CSS class
+const currentPage = document.location.pathname;
+
+let filename = currentPage.substring(currentPage.lastIndexOf('/')+1);
+filename = filename.substring(0, filename.length - 5);
+let navID = "nav-" + filename;
+
+const navElement = document.getElementById(navID);
+navElement.classList.add('active');
--- js/profile.js
+++ js/profile.js
@@ -6,27 +6,31 @@ function getProfileData (user) {
 	// Fetch the user's data
 	const userData = db.collection('userData').doc(user.uid);
 	
-	// Display the user's high-res photo
-	let photoURL = user.photoURL;
-	user.providerData.forEach((profile) => {
-		const provider = profile.providerId;
-		switch (provider) {
-			case 'google.com':
-				photoURL = user.photoURL.replace('s96-c', 's300-c');
-				break;
-			default:
-				alert ('Error upscaling profile image.');
-		}
-	});
-	document.getElementById('profile-img').innerHTML = `<img src="${photoURL}" alt="Your profile picture" class="profile-img">`;
+	// Display the user's high-res photo, if one exists
+	if (user.photoURL) {
+		let photoURL = user.photoURL;
+		user.providerData.forEach((profile) => {
+			const provider = profile.providerId;
+			switch (provider) {
+				case 'google.com':
+					photoURL = user.photoURL.replace('s96-c', 's300-c');
+					break;
+				default:
+					alert ('Error upscaling profile image.');
+			}
+		});
+		document.getElementById('profile-img').innerHTML = `<img src="${photoURL}" alt="Your profile picture" class="profile-img">`;
+	} else {
+		document.getElementById('profile-img').innerHTML = `<i class="bi bi-person-square default-profile-img"></i>`;
+	}
 	
 	// Display the user's name
-	document.getElementById('user-name').innerHTML = user.displayName;
+	document.getElementById('user-name').innerHTML = user.displayName + '<button type="button" class="btn btn-sm btn-link btn-edit" data-bs-toggle="modal" data-bs-target="#editDisplayNameModal"><i class="bi bi-pencil-square"></i></button>';
 	
 	// Display the user's account details
 	const details = document.getElementById('user-details');
 	let detailsContent = `<h4>Your information</h4>`;
-	detailsContent += `<div class="row"><div class="col-6"><p><strong>Email</strong></p></div><div class="col-6"><p>${user.email}</p></div></div>`;
+	detailsContent += `<div class="row"><div class="col-6"><p><strong>Email</strong></p></div><div class="col-6"><p>${user.email}<button type="button" class="btn btn-sm btn-link btn-edit" data-bs-toggle="modal" data-bs-target="#editEmailModal"><i class="bi bi-pencil-square"></i></button></p></div></div>`;
 	detailsContent += `<div class="row"><div class="col-6"><p><strong>Linked accounts</strong></p></div>`;
 	detailsContent += `<div class="col-6">`;
 	user.providerData.forEach((profile) => {
@@ -39,18 +43,20 @@ function getProfileData (user) {
 				alert ('Error getting linked accounts');
 		}
 	});
-	detailsContent += `</div></row>`;
+	detailsContent += `</div>`;
+	detailsContent += `<div class="col-12 text-center"><button class="btn btn-sm btn-link" onclick="resetPassword()">Send password reset email</button></div></row>`;
 	details.innerHTML = detailsContent;
 	
 	// Display the user's statistics and settings
 	userData.onSnapshot((doc) => {
         const settings = document.getElementById('user-settings');
 		const stats = document.getElementById('user-stats');
+		const deleteDiv = document.getElementById('deleteDiv');
 		const data = doc.data();
 		const sort = data.sort;
 		
 		let statsContent = `<h4>Statistics</h4>`;
-		statsContent += `<div class="row"><div class="col-6"><strong>Active challenges</strong></p></div><div class="col-6"><p>${data.challenges - data.completed}</p></div></div>`;
+		statsContent += `<div class="row"><div class="col-6"><strong>Active challenges</strong></p></div><div class="col-6"><p>${data.total_challenges - data.completed}</p></div></div>`;
 		statsContent += `<div class="row"><div class="col-6"><strong>Challenges completed</strong></p></div><div class="col-6"><p>${data.completed}</p></div></div>`;
 		statsContent += `<div class="row"><div class="col-6"><strong>Distance tracked</strong></p></div><div class="col-6"><p>${parseFloat(data.distance.toFixed(2))} ${data.unit}</p></div></div>`;
 		stats.innerHTML = statsContent;
@@ -66,8 +72,10 @@ function getProfileData (user) {
 		settingsContent += `<div class="col-10 mb-2 offset-1">`;
 		settingsContent += `<div class="form-check"><input class="form-check-input" type="radio" name="default-unit" value="miles" id="miles"><label class="form-check-label" for="miles">miles</label></div>`;
 		settingsContent += `<div class="form-check"><input class="form-check-input" type="radio" name="default-unit" value="kilometers" id="kilometers"><label class="form-check-label" for="kilometers">kilometers</label></div></div>`;
-		settingsContent += `<div class="text-center"><button class="btn btn-sm btn-primary" onclick="saveChanges()">Save changes</button></div>`;
+		settingsContent += `<div class="text-center"><button class="btn btn-primary" onclick="saveChanges()">Save changes</button></div>`;
 		settings.innerHTML = settingsContent;
+		
+		deleteDiv.innerHTML = '<hr><button class="btn btn-danger" data-bs-toggle="modal" data-bs-target="#deleteModal">Delete my account and data</button>';
 		
 		const sortMethods = settings.querySelectorAll('input[name="sort-by"]');
 		for (const sortMethod of sortMethods) {
@@ -98,7 +106,8 @@ function getUserData (user) {
 			userData.set({
 				sort: 'progress-desc',
 				unit: 'miles',
-				challenges: 0,
+				total_challenges: 0,
+				challeges: '',
 				completed: 0,
 				distance: 0
 			}).then(() => {
@@ -189,6 +198,81 @@ function saveChanges () {
     		toast.show();
 		}
     });
+}
+
+// Changes the user's displayName
+function changeName () {
+	// Get the new name entered
+	const newName = document.getElementById('newName').value;
+	
+	// If the user didn't enter a new name
+	if (newName == null) {
+		alert ('Please enter a new name.');
+		return;
+	}
+	
+	// Update the database with the new name
+	const user = firebase.auth().currentUser;
+	user.updateProfile({
+		displayName: newName
+	}).then(() => {
+		location.reload();
+	}).catch((error) => {
+		console.log ('Error updating name: ', error);
+	});
+}
+
+function changeEmail () {
+	// Get the new name entered
+	const newEmail = document.getElementById('newEmail').value;
+	
+	// If the user didn't enter a new name
+	if (newEmail == null) {
+		alert ('Please enter a new email address.');
+		return;
+	}
+	
+	const user = firebase.auth().currentUser;
+	
+	user.updateEmail(newEmail).then(() => {
+		location.reload();
+	}).catch((error) => {
+		alert ('Please sign in again to continue.');
+		location.href = './signin.html';
+	});
+}
+
+function resetPassword () {
+	const user = firebase.auth().currentUser;
+	const email = user.email;  
+	firebase.auth().sendPasswordResetEmail(email)
+	.then(() => {
+		const toastDiv = document.getElementById('toast-container');
+		toastDiv.innerHTML = `<div class="toast align-items-center" role="alert" aria-live="assertive" aria-atomic="true" id="resetToast"><div class="d-flex"><div class="toast-body">Password reset email sent.</div><button type="button" class="btn-close me-2 m-auto" data-bs-dismiss="toast" aria-label="Close"></button></div></div>`;
+		const resetToast = document.getElementById('resetToast');
+		const toast = new bootstrap.Toast(resetToast);
+		toast.show();
+	})
+	.catch((error) => {
+		console.log ('Error resetting password', error);
+	});
+}
+
+function deleteUser () {
+	const user = firebase.auth().currentUser;
+	const userData = db.collection('userData').doc(user.uid);
+	
+	userData.delete().then(() => {
+		user.delete().then(() => {
+			alert ('Your account and data has been deleted.');
+			location.href = './index.html';
+		}).catch((error) => {
+			alert ('Please sign in again to continue.');
+			location.href = './signin.html';
+		});
+	}).catch((error) => {
+		console.error('Error deleting user data: ', error);
+	});
 }
 
 // Check whether the user is logged in
