172f0c26e04a1a1b7cda84bd3be8ea0e7fac3a63
sdcervi <sdcervi@gmail.com>
2022-03-23 18:44:03 -0700

    New login flow
    Goes to dashboard instead of profile

--- js/login.js
+++ js/login.js
@@ -25,7 +25,7 @@ const uiConfig = {
 	},
 	// Will use popup for IDP Providers sign-in flow instead of the default, redirect.
 	signInFlow: 'popup',
-	signInSuccessUrl: './profile.html',
+	signInSuccessUrl: './dashboard.html',
 	signInOptions: [
 		// Leave the lines as is for the providers you want to offer your users.
 		firebase.auth.EmailAuthProvider.PROVIDER_ID,
